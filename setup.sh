a6bbdcb16520c7524221f3cadd1a84c913395ae8
Marcel Eyer <me@maersu.ch>
2026-02-17 00:05:58 +0100

    wip

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 git config --global --add safe.directory /data/openclaw-data
+git config --global user.email "hello@thehappylab.com"
+git config --global user.name "The Happy Lab"
+
 gh auth setup-git
 git pull
 openclaw gateway restart
